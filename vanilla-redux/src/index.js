5a7334aacc34df529bf9f3914101c0d115392f97
jisu <parangwavetraveller@gmail.com>
2024-04-01 01:35:03 +0900

    Actions
    reducer 외부에서 reducer와 소통하기 위해서 action을 전달해줘야 한다. action은 dispatch() 함수에 단순 object로 정해주면 된다.

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -8,12 +8,25 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.getElementById("number");
 
-// 나의 data를 modify(수정)하는 function
+// reducer = 나의 data를 modify(수정)하는 function
 // reducer가 return하는 것 = application에 있는 data
+
 // 1. reducer/modifier는 나의 data를 바꿔준다. = application에 있는 data
 // 만약에 여기에 state가 없으면, 0으로 초기화
-const countModifier = (count = 0) => {
-  return count;
+
+// 2. state를 어떻게 modify? action으로 수정하자
+// action은 plain한 obj여야함
+// reducer 외부에서 reducer와 소통할 수 있어야 함 -> action 보내기
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    // console.log("add one");
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
 const countStore = legacy_createStore(countModifier);
@@ -23,3 +36,16 @@ console.log(countModifier);
 console.log(countStore);
 // 4개의 함수가 있음
 console.log(countStore.getState());
+
+// dispatch()을 통해 action(메세지)을 보낼 수 있음
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "MINUS"});
+countStore.dispatch({type: "MINUS"});
+
+console.log(countStore.getState());
+// create data's store -> set data's modifier 
+// -> send message to the store -> set message in action and check it
