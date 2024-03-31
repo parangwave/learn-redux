14f0e547ec673f6de72a0b638771589d710ed1b8
jisu <parangwavetraveller@gmail.com>
2024-04-01 01:20:15 +0900

    Store & Reducer
    Store는 state를 저장, Reducer는 나의 data를 modify(수정)하는 함수로 이 수정한 data는 application의 data가 된다

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,28 +1,25 @@
 // Vanilla Redux
+
+import { legacy_createStore } from "redux" // npm install redux
+// store = 나의 state(data)를 넣는 곳
+// legacy_createStore으로 써야 취소선이 나타나지 않음
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.getElementById("number");
 
-let count = 0;
+// 나의 data를 modify(수정)하는 function
+// reducer가 return하는 것 = application에 있는 data
+// 1. reducer/modifier는 나의 data를 바꿔준다. = application에 있는 data
+// 만약에 여기에 state가 없으면, 0으로 초기화
+const countModifier = (count = 0) => {
+  return count;
+};
 
-number.innerText = count;
+const countStore = legacy_createStore(countModifier);
+// createStore()는 reducer() 함수를 요구
 
-const updateCount = () => {
-  number.innerText = count;
-}
-
-
-const handleAdd = () => {
-  // console.log("add");
-  count += 1;
-  updateCount();
-}
-
-const handleMinus = () => {
-  // console.log("minus");
-  count -= 1;
-  updateCount();
-}
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+console.log(countModifier);
+console.log(countStore);
+// 4개의 함수가 있음
+console.log(countStore.getState());
